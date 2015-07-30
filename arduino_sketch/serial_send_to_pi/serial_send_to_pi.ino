7d9f6d6078ef9edf62804cff0a0d7e0000f1f058
Andre Plante <adjplante@gmail.com>
2015-07-30 17:55:31 -0400

    Implement Web site with ethernet shield

--- arduino_sketch/serial_send_to_pi/serial_send_to_pi.ino
+++ arduino_sketch/serial_send_to_pi/serial_send_to_pi.ino
@@ -1,3 +1,15 @@
+#include <SPI.h>
+#include <Ethernet.h>
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = {
+  0x90, 0xA2, 0xDA, 0x0D, 0xA5, 0x5F
+};
+IPAddress ip(192, 168, 1, 3);
+// Initialize the Ethernet server library
+// with the IP address and port you want to use
+// (port 80 is default for HTTP):
+EthernetServer server(80);
 
 #include "Wire.h"
 #include "DHT.h"
@@ -126,7 +138,17 @@ void get_temp_HTU21DF(){
 }
 
 void setup(){
+  // Open serial communications and wait for port to open:
   Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+
   // initialize i2c as slave
   Wire.begin(SLAVE_ADDRESS);
   if( SENSOR_TYPE.equals("DHT22")){
@@ -209,5 +231,52 @@ void loop(){
   }else{
      delay(60000);
   }
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          // output the value of each analog input pin
+          client.println("iMeteo.ca<br />");     
+          client.println("Coin SE : temperature "+String(data[0])+" C, humidite "+String(data[1])+" %");
+	  client.println("<br>");
+          client.println("Coin SO : temperature "+String(data[2])+" C, humidite "+String(data[3])+" %");
+	  
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
+	
 }
 
